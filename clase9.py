82430f9cdf9dace0ecef92130cb46dbb383e4beb
brian ahumada <ahumada.yayan@gmail.com>
2023-08-19 01:19:41 -0300

    modificaciones

--- clase9.py
+++ clase9.py
@@ -8,7 +8,7 @@ from sklearn.decomposition import PCA
 
 data={
     'tipo_de_fruta':['manzana','banana','manzana','banana','naranja','manzana','naranja','manzana'],
-    'peso':[100, None, 300, 400, 500, 600, 700, 800],
+    'peso':[100, 200, 300, 400, 500, 600, 700, 800],
     'diametro':[7.5, 6.8, 7.2, 8.5, 6.3, 6.4, 6.6, 7.5],
     'sabor':[1, 2, 3, 4, 5, 6, 7, 8],
     'precio':[0.5, 0.7, 0.6, 0.3, 0.2, 0.1, 0.2, 0.5]
@@ -45,7 +45,13 @@ plt.title("distribucion de tipo de fruta")
 plt.show()
 
 # graficamos matriz de corrrlacion
-correlacion= df_frutas.corr()
+# Correccion estaba tomando columna tipo_de_fruta que es string 
+# lo que hacemos es tomar solo los float y int 
+
+# Excluir la columna 'tipo_de_fruta' del cálculo de la matriz de correlación
+columnas_numericas = df_frutas.select_dtypes(include=[float, int]).columns
+print(f"Columnas numericas-------------------\n{columnas_numericas}")
+correlacion= df_frutas[columnas_numericas].corr()
 print(correlacion)
 
 plt.figure(figsize=(8,6))
